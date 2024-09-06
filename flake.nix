3833e75edfb2a513cefda744871918c99e65ae7d
thebesttv <taojuntian@outlook.com>
2024-09-06 08:07:53 +0000

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+# nix develop --extra-experimental-features nix-command --extra-experimental-features flakes
+
+{
+  description = "b2p";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self , nixpkgs , ... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        figlet
+
+        python312
+        python312Packages.tqdm
+        python312Packages.pyyaml
+
+        ffmpeg_7-headless
+
+        php
+      ];
+
+      shellHook = ''
+        figlet b2p!
+      '';
+    };
+  };
+}
